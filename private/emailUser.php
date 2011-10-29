7a346c095bcb9c57ba7be727fbc578edb61a8c8c
Andrew Rivett <veggiefrog@gmail.com>
2011-10-29 15:54:04 -0400

    Added improvements to bug handling

---
+++ private/emailUser.php
@@ -0,0 +1,44 @@
+<?php
+//
+// Description
+// -----------
+// This function will send an email to a user of the system.
+// 
+// Info
+// ----
+// Status: beta
+//
+// Arguments
+// ---------
+//
+// Returns
+// -------
+//
+function ciniki_users_emailUser($ciniki, $user_id, $subject, $msg) {
+
+	//
+	// Query for user information
+	//
+	require_once($ciniki['config']['core']['modules_dir'] . '/core/private/dbQuote.php');
+	require_once($ciniki['config']['core']['modules_dir'] . '/core/private/dbHashQuery.php');
+	$strsql = "SELECT id, firstname, lastname, email "
+		. "FROM users "
+		. "WHERE id = '" . ciniki_core_dbQuote($ciniki, $user_id) . "' "
+		. "";
+	$rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'users', 'user');
+	if( $rc['stat'] != 'ok' || !isset($rc['user']) ) {
+		return array('stat'=>'fail', 'err'=>array('code'=>'993', 'msg'=>'Unable to find email information', 'err'=>$rc['err']));
+	}
+	$user = $rc['user'];
+
+	//  
+	// The from address can be set in the config file.
+	//  
+	$headers = 'From: "' . $ciniki['config']['core']['system.email.name'] . '" <' . $ciniki['config']['core']['system.email'] . ">\r\n" .
+		'Reply-To: "' . $ciniki['config']['core']['system.email.name'] . '" <' . $ciniki['config']['core']['system.email'] . ">\r\n" .
+		'X-Mailer: PHP/' . phpversion();
+	mail($user['email'], $subject, $msg, $headers, '-f' . $ciniki['config']['core']['system.email']);
+
+	return array('stat'=>'ok');
+}
+?>
---
+++ private/userList.php
@@ -0,0 +1,39 @@
+<?php
+//
+// Description
+// -----------
+// This function is used by other methods internally to get a list of users
+// and some information about them.
+//
+// Info
+// ----
+// Status: beta
+//
+// Arguments
+// ---------
+// api_key:
+// auth_token:
+//
+// Returns
+// -------
+// <users>
+//		<user id='1' email='' firstname='' lastname='' display_name='' perms=''/>
+// </users>
+//
+function ciniki_users_userList($ciniki, $container_name, $ids) {
+
+	if( !is_array($ids) ) {
+		return array('stat'=>'fail', 'err'=>array('code'=>'995', 'msg'=>'Invalid list of users'));
+	}
+
+	//
+	// Query for the business users
+	//
+	require_once($ciniki['config']['core']['modules_dir'] . '/core/private/dbQuoteIDs.php');
+	require_once($ciniki['config']['core']['modules_dir'] . '/core/private/dbRspQuery.php');
+	$strsql = "SELECT id, email, firstname, lastname, display_name, perms FROM users "
+		. "WHERE id IN (" . ciniki_core_dbQuoteIDs($ciniki, $ids) . ") ";
+		. "ORDER BY lastname, firstname ";
+	return ciniki_core_dbRspQuery($ciniki, $strsql, 'users', $container_name, 'user', array('stat'=>'ok', $container_name=>array()));
+}
+?>
