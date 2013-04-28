78662bd2e58c217dd2e3e819a0d5e3688a2f7dd5
Andrew Rivett <veggiefrog@gmail.com>
2013-04-28 01:00:53 -0400

    Added user timeFormat function

---
+++ private/timeFormat.php
@@ -0,0 +1,32 @@
+<?php
+//
+// Description
+// -----------
+//
+// Info
+// ----
+// Status: 			beta
+//
+// Arguments
+// ---------
+// user_id: 		The user making the request
+// 
+// Returns
+// -------
+//
+function ciniki_users_timeFormat($ciniki) {
+
+	//
+	// Check if the user is logged in, otherwise return 
+	//
+	if( isset($ciniki['session']['user']['settings']['time_format']) && $ciniki['session']['user']['settings']['time_format'] != '' ) {
+		return $ciniki['session']['user']['settings']['time_format'];
+	}
+
+	//
+	// This function does not return the standard response, because it will NEVER return an error.  
+	// If a problem is encountered, the it will return the default date format.
+	//
+	return "%H:%i %p";
+}
+?>
