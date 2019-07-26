736ce8eb34bc2f6884595188f8a0844fb63647c8
Andrew Rivett <veggiefrog@gmail.com>
2019-07-26 14:32:12 +0000

    Added check for single tenant mode

--- public/auth.php
+++ public/auth.php
@@ -141,9 +141,9 @@ function ciniki_users_auth(&$ciniki) {
     }
 
     //
-    // If the user is not a sysadmin, check if they only have access to one tenant
+    // In single tenant mode, or ff the user is not a sysadmin, check if they only have access to one tenant
     //
-    if( ($ciniki['session']['user']['perms'] & 0x01) == 0 ) {
+    if( $ciniki['config']['ciniki.core']['single_tenant_mode'] == 'yes' || ($ciniki['session']['user']['perms'] & 0x01) == 0 ) {
         $strsql = "SELECT DISTINCT ciniki_tenants.id, name "
             . "FROM ciniki_tenant_users, ciniki_tenants "
             . "WHERE ciniki_tenant_users.user_id = '" . ciniki_core_dbQuote($ciniki, $ciniki['session']['user']['id']) . "' "
